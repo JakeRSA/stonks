094bf4d8df7d7136be21f38e575202837c45e874
JakeRSA <jakenudels@gmail.com>
2020-11-10 16:04:21 +0200

    completed milestone 3

--- js/search.js
+++ js/search.js
@@ -5,6 +5,20 @@ const symbolInput = document.getElementById('stock-symbol');
 const resultsList = document.getElementById('results-list');
 const searchSpinner = document.querySelector('.lds-dual-ring');
 
+function getResultData(symbol) {
+    let response = fetch(`${finServer}/api/v3/company/profile/${symbol}`)
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        return {
+            img: data.profile.image.toString(),
+            percent: data.profile.changesPercentage
+        };
+    })
+    return response;
+}
+
 async function getResults(searchTerm) {
     searchSpinner.style.display = 'inline-block';
     searchBtn.style.display = 'none';
@@ -17,21 +31,41 @@ async function getResults(searchTerm) {
 
 function resultsToList(results) {
     if (!symbolInput.value) {
-        results = [];
+        return 1;
     }
     for (let result of results) {
         let resultLI = document.createElement('LI');
-        resultLI.innerHTML = 
-        `<a href="./company.html?symbol=${result.symbol}">
-        ${result.name} (${result.symbol})
-        </a>`;
+        let resultA = document.createElement('A');
+        let resultIMG = document.createElement('IMG');
+        let resultName = document.createElement('P');
+        let resultPercent = document.createElement('P');
+        
+        resultA.href = `./company.html?symbol=${result.symbol}`;
+        getResultData(result.symbol).then(resultData => {
+            resultIMG.src = resultData.img;
+            resultPercent.innerText = resultData.percent;
+            if (resultData.percent[1] === '+') {
+                resultPercent.classList += 'result-increase';
+            } else {
+                resultPercent.classList += 'result-decrease';
+            }
+
+        });
+        resultName.innerText = `${result.symbol} | ${result.name}`;
+
         resultLI.classList += 'result';
+        resultA.appendChild(resultIMG);
+        resultA.appendChild(resultName);
+        resultA.appendChild(resultPercent);
+        resultLI.appendChild(resultA);
         resultsList.appendChild(resultLI);
     }
+    resultsList.style.overflowY = 'scroll';
 }
 
 function clearResults() {
     resultsList.innerHTML = ''
+    resultsList.style.overflowY = 'hidden';
 }
 
 const debounce = (func, delay) => {
