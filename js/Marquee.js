23b25ebcf888f8e4b3a2f869e859e712a9e6b101
JakeRSA <jakenudels@gmail.com>
2020-11-11 15:40:27 +0200

    completed milestone 5 for real

--- js/Marquee.js
+++ js/Marquee.js
@@ -1,33 +1,49 @@
-const marquee = document.getElementById('marquee');
-const tickerList = document.getElementById('tickerItems');
+class Marquee {
 
-async function populateTicker() {
+    constructor(container) {
+        this.container = container;
+    }
+    
+    async loadData() {
+        let res = await fetch(`${finServer}/api/v3/stock/list`);
+        let data = await res.json();  
+        this.data = data;     
+    }
 
-    let res = await fetch(`${finServer}/api/v3/stock/list`);
-    let data = await res.json();
-    const ticker = document.createElement('DIV');
-    ticker.classList += 'ticker';
-    const tickerList = document.createElement('UL');
-    tickerList.classList += 'ticker-items';
-    marquee.append(ticker);
-    ticker.append(tickerList);
-    let nasdaqPriceys = [];
-    for (let stock of data) {
-        if (stock.exchange === 'Nasdaq Global Select' &&
-            stock.price > 40) {
-            nasdaqPriceys.push(stock);
-            let tickerItem = document.createElement('LI');
-            let itemSymbol = document.createElement('P');
-            itemSymbol.innerText = stock.symbol;
-            itemSymbol.classList += 'ticker-item-symbol';
-            let itemPrice = document.createElement('P');
-            itemPrice.innerText = stock.price;
-            itemPrice.classList += 'ticker-item-price';
-            tickerItem.appendChild(itemSymbol);
-            tickerItem.appendChild(itemPrice);
-            tickerList.append(tickerItem);
-        }
+    createEmptyTicker() {
+        const ticker = document.createElement('DIV');
+        ticker.classList += 'ticker';
+        const tickerList = document.createElement('UL');
+        tickerList.classList += 'ticker-items';
+        this.container.append(ticker);
+        ticker.append(tickerList);
+        return tickerList;      
+    }
+
+    populateTicker(tickerList) {
+        let nasdaqPriceys = [];
+        for (let stock of this.data) {
+            if (stock.exchange === 'Nasdaq Global Select' &&
+                stock.price > 40) {
+                nasdaqPriceys.push(stock);
+                let tickerItem = document.createElement('LI');
+                let itemSymbol = document.createElement('P');
+                itemSymbol.innerText = stock.symbol;
+                itemSymbol.classList += 'ticker-item-symbol';
+                let itemPrice = document.createElement('P');
+                itemPrice.innerText = stock.price;
+                itemPrice.classList += 'ticker-item-price';
+                tickerItem.appendChild(itemSymbol);
+                tickerItem.appendChild(itemPrice);
+                tickerList.append(tickerItem);
+            }
+        }        
+    }
+
+    async load() {
+        await this.loadData();
+        let tickerList = this.createEmptyTicker();
+        this.populateTicker(tickerList);
     }
 }
 
-populateTicker();
