f86b80f555918973b1a466eef6b04bf4eac1a62d
JakeRSA <jakenudels@gmail.com>
2020-11-16 20:13:55 +0200

    completed milestone 8

--- js/searchResult.js
+++ js/searchResult.js
@@ -47,20 +47,28 @@ class SearchResult {
         return innerHTML;
     }
 
+    compareListener(button, companyData) {
+        button.addEventListener('click', () => {
+            console.log(companyData)
+        })
+    }
+
     listResults(results) {
-        console.log(results.data);
         this.resultsList.innerHTML = '';
         this.resultsList.classList.remove('enable-scroll');
         if (results.data.length < 1) return 1;
 
         for (let result of results.data) {
             let resultLI = document.createElement('LI');
-            let resultA = document.createElement('A');
+            let resultDIV = document.createElement('DIV');
             let resultIMG = document.createElement('IMG');
-            let resultName = document.createElement('P');
-            let resultPercent = document.createElement('P');
+            let resultName = document.createElement('A');
+            let resultPercent = document.createElement('A');
+            let resultCompare = document.createElement('BUTTON');
 
-            resultA.href = `./company.html?symbol=${result.symbol}`;
+            resultDIV.classList.add('result-body');
+            resultName.href = `./company.html?symbol=${result.symbol}`;
+            resultPercent.href = `./company.html?symbol=${result.symbol}`;
             this.getResultData(result.symbol).then(resultData => {
                 resultIMG.src = resultData.img;
                 resultPercent.innerText = resultData.percent;
@@ -74,12 +82,15 @@ class SearchResult {
                 let highlightSymbol = this.highlightSearchTerm(result.symbol, results.searchTerm);
                 let highlightName = this.highlightSearchTerm(result.name, results.searchTerm);
                 resultName.innerHTML = `${highlightSymbol} | ${highlightName}`;
-
+                resultCompare.innerText = 'compare';
+                resultCompare.classList.add('compare-btn');
+                this.compareListener(resultCompare, result);
                 resultLI.classList.add('result');
-                resultA.appendChild(resultIMG);
-                resultA.appendChild(resultName);
-                resultA.appendChild(resultPercent);
-                resultLI.appendChild(resultA);
+                resultDIV.appendChild(resultIMG);
+                resultDIV.appendChild(resultName);
+                resultDIV.appendChild(resultPercent);
+                resultLI.appendChild(resultDIV);
+                resultLI.appendChild(resultCompare);
                 this.resultsList.appendChild(resultLI);
             } catch (e) {
                 console.log(`Cannot load result: ${e}`)
